0fa76ae7be0f64e946733baf981ae9cf23f7c40a
Ciril Plackal <mail.cirilplackal@gmail.com>
2024-08-18 03:44:17 +0530

    completed dishes,navbar and about sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import HeroSection from "@components/HeroSection";
 import Navbar from "@components/Navbar";
+import Dishes from "@components/Dishes"
+import About from "@components/About";
 function App() {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
       <Navbar />
+      <Dishes/>
+      <About/>
     </main>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
     },
   },
 });
