f73adb2963900bd7a80abf358f293b09a5a93107
Ciril Plackal <mail.cirilplackal@gmail.com>
2024-08-18 12:50:44 +0530

    added expertise section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import HeroSection from "@components/HeroSection";
 import Navbar from "@components/Navbar";
-import Dishes from "@components/Dishes"
+import Dishes from "@components/Dishes";
 import About from "@components/About";
+import Mission from "@components/Mission";
+import Expertise from "@components/Expertise";
 function App() {
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
       <Navbar />
-      <Dishes/>
-      <About/>
+      <Dishes />
+      <About />
+      <Mission />
+    <Expertise/>
     </main>
   );
 }
